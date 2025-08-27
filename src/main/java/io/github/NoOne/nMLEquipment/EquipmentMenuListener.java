afbf7f6a56966e0bf0c4e82a3d6b6445b51acdc4
NoOne_NoBody <hypixelnerd347@gmail.com>
2025-08-27 09:55:33 -0400

    updated to not change armor automatically

--- src/main/java/io/github/NoOne/nMLEquipment/EquipmentMenuListener.java
+++ src/main/java/io/github/NoOne/nMLEquipment/EquipmentMenuListener.java
@@ -52,9 +52,8 @@ public class EquipmentMenuListener implements Listener {
     }
 
     @EventHandler()
-    public void openMenuWhenShiftEquippingArmor(InventoryClickEvent event) {
+    public void openMenuWhenShiftEquippingEquipment(InventoryClickEvent event) {
         Player player = (Player) event.getWhoClicked();
-        PlayerInventory playerInventory = player.getInventory();
         ItemStack currentItem = event.getCurrentItem();
         ClickType clickType = event.getClick();
         int clickedSlot = event.getSlot();
@@ -62,45 +61,31 @@ public class EquipmentMenuListener implements Listener {
         if (clickType.isShiftClick()) {
             if (clickedSlot >= 36 && clickedSlot <= 40) { // potentially armor off
                 event.setCancelled(true);
-                playerInventory.setItem(clickedSlot, null);
-                playerInventory.addItem(currentItem);
 
                 Bukkit.getScheduler().runTaskLater(nmlEquipment, () -> {
                     new EquipmentMenu(MenuSystem.getPlayerMenuUtility(player), nmlEquipment).open();
                 }, 1L);
-
-                Bukkit.getPluginManager().callEvent(new ArmorChangeEvent(player, currentItem, new ItemStack(Material.AIR)));
             } else { // putting armor on
-                if (currentItem.hasItemMeta() && (ItemSystem.getItemType(currentItem) == HELMET || ItemSystem.getItemType(currentItem) == CHESTPLATE ||
-                        ItemSystem.getItemType(currentItem) == LEGGINGS || ItemSystem.getItemType(currentItem) == BOOTS ||
-                        ItemSystem.getItemType(currentItem) == SHIELD) && ItemSystem.isItemUsable(currentItem, player)) {
+                if ((ItemSystem.isEquippable(currentItem)) && ItemSystem.isItemUsable(currentItem, player)) {
+                    event.setCancelled(true);
 
                     Bukkit.getScheduler().runTaskLater(nmlEquipment, () -> {
                         new EquipmentMenu(MenuSystem.getPlayerMenuUtility(player), nmlEquipment).open();
                     }, 1L);
-
-                    Bukkit.getPluginManager().callEvent(new ArmorChangeEvent(player, new ItemStack(Material.AIR), currentItem));
                 }
             }
         }
     }
 
     @EventHandler
-    public void usingArmorInYourMainHand(PlayerInteractEvent event) {
+    public void usingEquipmentInYourMainHand(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         ItemStack heldItem = event.getItem();
         Action actionType = event.getAction();
 
         if (actionType == Action.RIGHT_CLICK_AIR || actionType == Action.RIGHT_CLICK_BLOCK) {
-            if (heldItem != null && (ItemSystem.getItemType(heldItem) == HELMET || ItemSystem.getItemType(heldItem) == CHESTPLATE ||
-                ItemSystem.getItemType(heldItem) == LEGGINGS || ItemSystem.getItemType(heldItem) == BOOTS)) {
-
-                switch (ItemSystem.getItemType(heldItem)) {
-                    case HELMET -> Bukkit.getPluginManager().callEvent(new ArmorChangeEvent(player, player.getInventory().getHelmet(), heldItem));
-                    case CHESTPLATE -> Bukkit.getPluginManager().callEvent(new ArmorChangeEvent(player, player.getInventory().getChestplate(), heldItem));
-                    case LEGGINGS -> Bukkit.getPluginManager().callEvent(new ArmorChangeEvent(player, player.getInventory().getLeggings(), heldItem));
-                    case BOOTS -> Bukkit.getPluginManager().callEvent(new ArmorChangeEvent(player, player.getInventory().getBoots(), heldItem));
-                }
+            if (ItemSystem.isEquippable(heldItem)) {
+                event.setCancelled(true);
 
                 Bukkit.getScheduler().runTaskLater(nmlEquipment, () -> {
                     new EquipmentMenu(MenuSystem.getPlayerMenuUtility(player), nmlEquipment).open();
@@ -110,15 +95,13 @@ public class EquipmentMenuListener implements Listener {
     }
 
     @EventHandler
-    public void dontSwapArmorToOffhand(PlayerSwapHandItemsEvent event) {
+    public void dontSwapEquipmentToOffhand(PlayerSwapHandItemsEvent event) {
         Player player = event.getPlayer();
         ItemStack mainhand = event.getOffHandItem();
 
-        if (ItemSystem.getItemType(mainhand) == HELMET || ItemSystem.getItemType(mainhand) == CHESTPLATE ||
-                ItemSystem.getItemType(mainhand) == LEGGINGS || ItemSystem.getItemType(mainhand) == BOOTS
-                || ItemSystem.getItemType(mainhand) == SHIELD) {
-
+        if (ItemSystem.isEquippable(mainhand)) {
             event.setCancelled(true);
+
             Bukkit.getScheduler().runTaskLater(nmlEquipment, () -> {
                 new EquipmentMenu(MenuSystem.getPlayerMenuUtility(player), nmlEquipment).open();
             }, 1L);
